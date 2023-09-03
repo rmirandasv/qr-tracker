f1ec32f601989ff0b45510c6051adfd5459079ff
Ronald Miranda <ronaldmirandasv@gmail.com>
2023-09-03 02:49:25 -0600

    replace qr lib

--- app/Livewire/Components/Qr/QrGenerator.php
+++ app/Livewire/Components/Qr/QrGenerator.php
@@ -2,14 +2,28 @@
 
 namespace App\Livewire\Components\Qr;
 
-use chillerlan\QRCode\QRCode;
-use chillerlan\QRCode\QROptions;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Writer\SvgWriter;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class QrGenerator extends Component
 {
+    use WithFileUploads;
+
     public $input;
     public $output;
+    public $size = 300;
+    public $margin = 10;
+    public $label = 'Scan the code';
+    public $labelFontSize = 20;
+    public $logo;
 
     public function mount($input)
     {
@@ -18,15 +32,29 @@ class QrGenerator extends Component
 
     public function render()
     {
-        $options = new QROptions([
-            'version' => 5,
-            'outputType' => QRCode::OUTPUT_MARKUP_SVG,
-            'eccLevel' => QRCode::ECC_L,
-            'bgColor' => '#000000',
-            'quietzoneSize' => 4,
-        ]);
-        $this->output = (new QRCode($options))->render($this->input);
+        $this->output = Builder::create()
+            ->writer(new SvgWriter())
+            ->writerOptions([])
+            ->data($this->input)
+            ->encoding(new Encoding('UTF-8'))
+            ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+            ->size($this->size)
+            ->margin($this->margin)
+            ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+            ->labelText($this->label)
+            ->labelFont(new NotoSans($this->labelFontSize))
+            ->labelAlignment(new LabelAlignmentCenter())
+            ->validateResult(false)
+        ;
 
+        if ($this->logo) {
+            $this->output->logoPath($this->logo->getRealPath())
+                ->logoResizeToWidth(50)
+                ->logoResizeToHeight(50)
+            ;
+        }
+
+        $this->output = $this->output->build()->getDataUri();
         return view('livewire.components.qr.qr-generator');
     }
 }
--- resources/views/livewire/components/qr/qr-generator.blade.php
+++ resources/views/livewire/components/qr/qr-generator.blade.php
@@ -1,3 +1,36 @@
 <div>
-    <img class="w-48 h-48" src="{{ $output }}" alt="QR Code" />
+    <div class="flex justify-between">
+        <div class="w-4/12 flex flex-col">
+            <div class="flex flex-col">
+                <label for="size" class="text-sm font-bold text-gray-700 tracking-wide">{{ __('Size') }}</label>
+                <select wire:model.live="size" id="size" name="size" class="w-full mt-2">
+                    <option value="100">100</option>
+                    <option value="200">200</option>
+                    <option value="300">300</option>
+                </select>
+            </div>
+            <div class="flex flex-col mt-4">
+                <label for="margin" class="text-sm font-bold text-gray-700 tracking-wide">{{ __('Margin') }}</label>
+                <select wire:model.live="margin" id="margin" name="margin" class="w-full mt-2">
+                    <option value="0">0</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="30">30</option>
+                </select>
+            </div>
+            <div class="flex flex-col mt-4">
+                <x-input wire:model.live="label" id="label" name="label" type="text" placeholder="{{ __('Scan the code') }}" />
+            </div>
+            <div class="flex flex-col mt-4">
+                <x-input wire:model.live="labelFontSize" id="labelFontSize" name="labelFontSize" type="number" step="1" placeholder="{{ __('Label text size') }}" />
+            </div>
+            <div class="flex flex-col mt-4">
+                <label for="logo" class="text-sm font-bold text-gray-700 tracking-wide">{{ __('Logo') }}</label>
+                <input wire:model.live="logo" id="logo" name="logo" type="file" />
+            </div>
+        </div>
+        <div class="w-auto flex flex-col">
+            <img src="{{ $output }}" alt="QR Code" />
+        </div>
+    </div>
 </div>
