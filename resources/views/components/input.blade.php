c9900394926578aef90f4bfa21f3a4910ac06962
Ronald Miranda <ronaldmirandasv@gmail.com>
2023-09-11 00:32:51 -0600

    fix form validation error

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,7 +1,7 @@
 @props(['disabled' => false])
 <div class="flex flex-col">
     <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-0 focus:border-gray-300']) !!}>
-    @error($attributes->get('wire:model') ?? $attributes->get('name'))
+    @error($attributes->get('name') ?? $attributes->get('id'))
         <p class="mt-1 text-xs text-red-500">{{ $message }}</p>
     @enderror
 </div>
